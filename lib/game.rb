aa1fc454a62f462b1101407d6b2dbb7f70632e22
Aljun D. Rendon <aljunrendon@yahoo.com>
2025-05-01 22:28:02 +0800

    refactor #play test inorder to take account of #display_board

--- lib/game.rb
+++ lib/game.rb
@@ -4,17 +4,68 @@ require_relative './board'
 
 # Game class
 class Game
+  attr_reader :current_player
+
   def initialize(board)
-    @board = Board.new
+    @board = board
     @players = %W[X O]
     @current_player = @players.first
   end
 
   def play
-    
+    until game_over?
+      display_board
+      play_turn
+      switch_players
+    end
+    display_result
   end
 
   def switch_players
     @current_player = @players.rotate!.first
+  end
+
+  def play_turn
+    loop do
+      column = prompt_column
+      if @board.drop_disc(column, @current_player)
+        break
+      else
+        puts "Column #{column} is invalid or full. Try again."
+      end
+    end
+  end
+
+  private
+
+  def game_over?
+    @board.winner? || @board.full?
+  end
+
+  def display_board
+    @board.grid.each do |row|
+      puts row.join('')
+    end
+    puts "\n"
+  end
+
+  def prompt_column
+    loop do
+      print "Player #{@current_player} please enter a column (0-6): "
+      input = gets.chomp
+      column = input.to_i
+      return column if (0..6).cover?(column)
+
+      puts "Invalid column. Please enter a number between 0 and 6."
+    end
+  end
+
+  def display_result
+    display_board
+    if @board.winner?
+      puts "Player #{@board.winner?} WINS! What a CHAD!"
+    else
+      puts "It's a TIE!"
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,8 +11,37 @@ RSpec.describe Game do
     it 'switches the current player' do
       player_1 = game.instance_variable_get(:@current_player)
       game.switch_players
-      expect(game.instance_variable_get(:@current_player)).not_to eq(player_1)
-      expect(game.instance_variable_get(:@current_player)).to eq('O')
+      expect(game.instance_variable_get(:@current_player))
+        .not_to eq(player_1)
+      expect(game.instance_variable_get(:@current_player))
+        .to eq('O')
+    end
+  end
+
+  describe '#play' do
+    before do
+      allow(game).to receive(:display_board)
+    end
+
+    context 'when a player wins' do
+      it 'announces the winner' do
+        allow(game).to receive(:game_over?).and_return true
+        allow(board).to receive(:winner?).and_return('X')
+        expect {game.play}
+          .to output("Player X WINS! What a CHAD!\n")
+          .to_stdout
+      end
+    end
+
+    context 'when the board is full' do
+      it 'announces a tie' do
+        allow(game).to receive(:display_board)
+        allow(board).to receive(:winner?).and_return(nil)
+        allow(board).to receive(:full?).and_return true
+        expect { game.play }
+          .to output("It's a TIE!\n")
+          .to_stdout
+      end
     end
   end
 end
