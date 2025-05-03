2378591d61b13dd691655edc1baaae6b69d7f4dc
Aljun D. Rendon <aljunrendon@yahoo.com>
2025-05-03 12:56:03 +0800

    add game instruction file

---
+++ lib/how_to_play.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+def instructions
+  <<-DOC
+    Welcome to CONNECT FOUR!!
+
+    The Game:
+
+    Player 1 has discs marked ('X') while Player 2 has discs marked ('O'), first player to CONNECT FOUR IDENTICAL DISCS:
+
+    a. X X X X
+    
+    b. X
+       X
+       X
+       X
+
+    c. X
+         X
+           X
+             X
+
+    d.       X
+           X  
+         X
+       X
+    of either patterns a-b WINS.
+
+    How to place disc:
+
+    Select a column no. from 0-6 to drop a disc.
+  DOC
+end
